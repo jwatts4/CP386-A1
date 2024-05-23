838a780bf50af08be25cf403c74b3daa2b0be346
Jake Watts <jaketimothywatts@gmail.com>
2024-05-23 11:42:40 -0400

    Start over with filecopy.c
    Import a bunch of stuff.

--- filecopy.c
+++ filecopy.c
@@ -1,7 +1,10 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
     printf("Hello world!\n");
